581867940c02e4abfec1fec3681e8b0cc20958c8
Pablo Flores <a20203651@pucp.edu.pe>
2023-06-08 21:01:32 -0500

    Beans_partido_arbitro

---
+++ Lab7_20192832_20203651/src/main/java/com/example/lab7_20192832_20203651/ModelsLab/BeansLab/Arbitro.java
@@ -0,0 +1,34 @@
+package com.example.lab7_20192832_20203651.ModelsLab.BeansLab;
+
+public class Arbitro {
+
+    private int idArbitro;
+
+    private String nombreArbitro;
+
+    private String paisArbitro;
+
+    public int getIdArbitro() {
+        return idArbitro;
+    }
+
+    public void setIdArbitro(int idArbitro) {
+        this.idArbitro = idArbitro;
+    }
+
+    public String getNombreArbitro() {
+        return nombreArbitro;
+    }
+
+    public void setNombreArbitro(String nombreArbitro) {
+        this.nombreArbitro = nombreArbitro;
+    }
+
+    public String getPaisArbitro() {
+        return paisArbitro;
+    }
+
+    public void setPaisArbitro(String paisArbitro) {
+        this.paisArbitro = paisArbitro;
+    }
+}
